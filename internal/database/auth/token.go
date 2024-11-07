a1a7498567b7fb0f55ad93d9dbc93a9147924537
Rizki <rizkidn17@gmail.com>
2024-11-07 15:05:29 +0700

    Create token auth

---
+++ internal/database/auth/token.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"log"
+	"os"
+	"time"
+)
+
+var secretKey = []byte(os.Getenv("JWT_SECRET_KEY"))
+
+func GenerateToken(username string, email string) (string, error) {
+	// Create a new JWT token with claims
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"usr": username,                      // Subject (user identifier)
+		"eml": email,                         // User email
+		"iss": "golang-microservice-sekolah", // Issuer
+		"exp": time.Now().AddDate(1, 0, 0),   // Expiration time
+		"iat": time.Now().Unix(),             // Issued at
+	})
+	
+	tokenString, err := claims.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+	
+	// Print information about the created token
+	log.Printf("Token claims added: %+v\n", claims)
+	return tokenString, nil
+}
