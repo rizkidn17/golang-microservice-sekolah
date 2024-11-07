f0919fe8146f82d5df49c9c657ca5ca793de4130
Rizki <rizkidn17@gmail.com>
2024-11-07 15:49:53 +0700

    Fix missed unix converter when generating token

--- internal/database/auth/token.go
+++ internal/database/auth/token.go
@@ -12,11 +12,11 @@ var secretKey = []byte(os.Getenv("JWT_SECRET_KEY"))
 func GenerateToken(username string, email string) (string, error) {
 	// Create a new JWT token with claims
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"usr": username,                      // Subject (user identifier)
-		"eml": email,                         // User email
-		"iss": "golang-microservice-sekolah", // Issuer
-		"exp": time.Now().AddDate(1, 0, 0),   // Expiration time
-		"iat": time.Now().Unix(),             // Issued at
+		"usr": username,                           // Subject (user identifier)
+		"eml": email,                              // User email
+		"iss": "golang-microservice-sekolah",      // Issuer
+		"exp": time.Now().AddDate(1, 0, 0).Unix(), // Expiration time
+		"iat": time.Now().Unix(),                  // Issued at
 	})
 	
 	tokenString, err := claims.SignedString(secretKey)
